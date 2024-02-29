a1fd7f94ca54fc8a98103b6084916d9f7c7475fa
Franklyn Roberto da Silva <franklyn1998@live.com>
2024-02-29 18:06:27 -0300

    feat - Salvando a imagem através da API

--- imageliteapi/src/main/java/com/github/hadesfranklyn/project/domain/enums/ImageExtension.java
+++ imageliteapi/src/main/java/com/github/hadesfranklyn/project/domain/enums/ImageExtension.java
@@ -1,8 +1,29 @@
 package com.github.hadesfranklyn.project.domain.enums;
 
+import java.util.Arrays;
+
+import org.springframework.http.MediaType;
+
 public enum ImageExtension {
-	PNG,
-	JPG,
-	GIF,
-	JPEG
+	PNG(MediaType.IMAGE_PNG), GIF(MediaType.IMAGE_GIF), JPEG(MediaType.IMAGE_JPEG);
+
+	private MediaType mediaType;
+
+	private ImageExtension(MediaType mediaType) {
+		this.mediaType = mediaType;
+	}
+
+	public static ImageExtension valueOf(MediaType mediaType) {
+		return Arrays.stream(values()).filter(ie -> ie.mediaType
+				.equals(mediaType)).findFirst().orElse(null);
+	}
+
+	public MediaType getMediaType() {
+		return mediaType;
+	}
+
+	public void setMediaType(MediaType mediaType) {
+		this.mediaType = mediaType;
+	}
+
 }
--- imageliteapi/src/main/java/com/github/hadesfranklyn/project/images/ImagesController.java
+++ imageliteapi/src/main/java/com/github/hadesfranklyn/project/images/ImagesController.java
@@ -1,7 +1,9 @@
 package com.github.hadesfranklyn.project.images;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,20 +12,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.github.hadesfranklyn.project.domain.entity.Image;
+import com.github.hadesfranklyn.project.domain.enums.ImageExtension;
+import com.github.hadesfranklyn.project.domain.service.ImageService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/v1/images")
 @CrossOrigin("*")
 @Slf4j
+@RequiredArgsConstructor
 public class ImagesController {
+	
+	private final ImageService service;
 
 	@PostMapping
-	public ResponseEntity<?> save(@RequestParam("file") MultipartFile file, @RequestParam("name") String name,
-			@RequestParam("tags") List<String> tags) {
+	public ResponseEntity<?> save(
+			@RequestParam("file") MultipartFile file, 
+			@RequestParam("name") String name,
+			@RequestParam("tags") List<String> tags
+			) throws IOException {
 		log.info("Imagem recebida: name: {}, size: {} ", file.getOriginalFilename(), file.getSize());
-		log.info("Nome defenido para a imagem: {}", name);
-		log.info("Tags: {}", tags);
+		
+		Image image = Image.builder()
+				.name(name)
+				.tags(String.join(",", tags))
+				.size(file.getSize())
+				.extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
+				.file(file.getBytes())
+				.build();
+		
+		service.save(image);
+		
 		return ResponseEntity.ok().build();
 	}
 }
