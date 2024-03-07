d5ccb69e54a48bc65f0dd5c62a201761e501a615
Franklyn Roberto da Silva <franklyn1998@live.com>
2024-03-07 11:54:44 -0300

    feat - Componente ImageCard e Página galeria

--- imagelite/src/app/galeria/page.tsx
+++ imagelite/src/app/galeria/page.tsx
@@ -1,9 +1,52 @@
-import { Template } from "../../components/Template";
+import { ImageCard, Template } from "@/components";
 
 export default function GaleriaPage() {
   return (
     <Template>
-      <h1>Galeria</h1>
+      <section className="grid grid-cols-4 gap-8">
+        <ImageCard
+          nome="Matrix"
+          tamanho="10Mb"
+          dataUpload="07/03/2024"
+          src="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg"
+        />
+        <ImageCard
+          nome="Matrix"
+          tamanho="10Mb"
+          dataUpload="07/03/2024"
+          src="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg"
+        />
+        <ImageCard
+          nome="Matrix"
+          tamanho="10Mb"
+          dataUpload="07/03/2024"
+          src="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg"
+        />
+        <ImageCard
+          nome="Matrix"
+          tamanho="10Mb"
+          dataUpload="07/03/2024"
+          src="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg"
+        />
+        <ImageCard
+          nome="Matrix"
+          tamanho="10Mb"
+          dataUpload="07/03/2024"
+          src="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg"
+        />
+        <ImageCard
+          nome="Matrix"
+          tamanho="10Mb"
+          dataUpload="07/03/2024"
+          src="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg"
+        />
+        <ImageCard
+          nome="Matrix"
+          tamanho="10Mb"
+          dataUpload="07/03/2024"
+          src="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg"
+        />
+      </section>
     </Template>
   );
 }
