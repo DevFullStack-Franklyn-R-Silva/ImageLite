f92c587efd5390a291167380565d1e2a31a8f15a
Franklyn Roberto da Silva <franklyn1998@live.com>
2024-03-07 14:22:13 -0300

    feat - Iniciando as melhorias na tela de pesquisa de imagens

--- imagelite/src/components/ImageCard.tsx
+++ imagelite/src/components/ImageCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 interface ImageCardProps {
   nome?: string;
   tamanho?: string;
@@ -11,6 +13,10 @@ export const ImageCard: React.FC<ImageCardProps> = ({
   dataUpload,
   src,
 }: ImageCardProps) => {
+  function download() {
+    window.open(src, "_blank");
+  }
+
   return (
     <div className="card relative bg-white rounded-md shadow-md transition-transform ease-in duration-300 transform hover:shadow-lg hover:-translate-y-2">
       <img src={src} alt="" className="h-56 w-full object-fill rounded-t-md" />
@@ -18,6 +24,20 @@ export const ImageCard: React.FC<ImageCardProps> = ({
         <h5 className="text-xl font-semibold mb-2 text-gray-600">{nome}</h5>
         <p className="text-gray-600">{tamanho}</p>
         <p className="text-gray-600">{dataUpload}</p>
+        <svg
+          onClick={download}
+          className="h-8 w-8 text-gray-400  hover:text-black"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+          />
+        </svg>
       </div>
     </div>
   );
