85a59eee06ab554721f74d6c1f67c899c15df3e4
Franklyn Roberto da Silva <franklyn1998@live.com>
2024-03-07 13:23:44 -0300

    feat - Trazendo as imagens através do front-end

--- imagelite/src/app/galeria/page.tsx
+++ imagelite/src/app/galeria/page.tsx
@@ -1,49 +1,31 @@
 "use client";
 
 import { ImageCard, Template } from "@/components";
+import { Image } from "@/resources/image/image.resource";
+import { useImageService } from "@/resources/image/image.service";
 import { useState } from "react";
 
 export default function GaleriaPage() {
-  const image1 =
-    "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/yDI6D5ZQh67YU4r2ms8qcSbAviZ.jpg";
-  const image2 =
-    "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/36LnijfQCOC89rCMOhn2OINXROI.jpg";
-  const nome1 = "Titanic";
-  const nome2 = "Star Wars";
+  const useService = useImageService();
+  const [images, setImages] = useState<Image[]>([]);
 
-  const [codigoImage, setCodigoImage] = useState<number>(2);
-  const [urlImage, setUrlImage] = useState<string>(image1);
-  const [nomeImage, setNomeImage] = useState<string>(nome1);
-
-  function mudarImagem() {
-    if (codigoImage == 1) {
-      setCodigoImage(2);
-      setUrlImage(image2);
-      setNomeImage(nome2);
-    } else {
-      setCodigoImage(1);
-      setUrlImage(image1);
-      setNomeImage(nome1);
-    }
+  async function searchImages() {
+    const result = await useService.buscar();
+    setImages(result);
+    console.table(images);
   }
 
   return (
     <Template>
-      <button className="bg-gray-500" onClick={mudarImagem}>
+      <button className="bg-gray-500" onClick={searchImages}>
         Clica para mudar
       </button>
       <section className="grid grid-cols-4 gap-8">
         <ImageCard
-          nome="Matrix"
+          nome="{nomeImage}"
           tamanho="10Mb"
           dataUpload="07/03/2024"
-          src="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg"
-        />
-        <ImageCard
-          nome={nomeImage}
-          tamanho="10Mb"
-          dataUpload="07/03/2024"
-          src={urlImage}
+          src="{urlImage}"
         />
       </section>
     </Template>
